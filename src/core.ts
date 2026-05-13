dfa50b7c0373244c09080965b12932f70dbe0166
Camila Moreno <camila.silva@estudantes.ifc.edu.br>
2026-05-13 19:50:31 +0000

    commitando dinooooooovo

---
+++ src/core.ts
@@ -0,0 +1,12 @@
+class item {}
+
+class Todo{
+    private items: item[]=[];
+    private filePath: string;
+
+    constructor(filePath: string) {
+        this.filePath = filePath;
+    }
+}
+
+const cams = new Todo(*arquivo.txt*)
