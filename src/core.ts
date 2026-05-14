3572458679b335379ec0a071bb8f5c746f0649f5
Camila Moreno <camila.silva@estudantes.ifc.edu.br>
2026-05-14 12:48:21 +0000

    botando as funcao

--- src/core.ts
+++ src/core.ts
@@ -1,12 +1,41 @@
-class item {}
+// class Item { 
+//   public title: string
+//   constructor(title: string) {
+//     this.title = title;
+//   }
+// }
+// diferenças.
 
-class Todo{
-    private items: item[]=[];
-    private filePath: string;
 
-    constructor(filePath: string) {
-        this.filePath = filePath;
-    }
+class Item {
+  constructor(public title: string) { }
 }
 
-const cams = new Todo(*arquivo.txt*)
+class TodoList {
+  private itens: Item[] = [];
+  private filePath: string;
+
+  constructor(filePath: string) {
+    this.filePath = filePath;
+  }
+
+  addItem(item: Item) {
+    this.itens.push(item)
+  }
+
+  removeItem(index: number) {
+    this.itens.splice(index, 1);
+  }
+
+  getItems() {
+    return this.itens
+  }
+}
+
+
+const lista = new TodoList('arquivo.txt')
+lista.addItem(new Item("ver ele"))
+lista.addItem(new Item("desenhar"))
+lista.removeItem(1)
+lista.addItem(new Item("pintar"))
+console.log(lista.getItems())
